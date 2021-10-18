6506f730b7b952eda20cb875e63c7fb888bbfb0e
Maximin Joshua <maximinusjoshus@gmail.com>
2021-10-18 21:13:08 +0530

    nothing

--- colour_filtering.py
+++ colour_filtering.py
@@ -1,12 +1,15 @@
 import cv2 as cv
 import numpy as np
+# import argparse
 
-img = cv.imread("D://medium_blogs//red_coat.jpg")
+# parser = argparse.ArgumentParser()
+# parser.add_argument()
+
+img = cv.imread("D://medium_blogs//colours1.jpg")
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-lower_red = np.array([160,100,50])
-upper_red = np.array([180,255,255])
-
+lower_red = np.array([80,100,50])
+upper_red = np.array([120,255,255])
 mask = cv.inRange(hsv, lower_red, upper_red)
 mask_inv = cv.bitwise_not(mask)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -15,21 +18,21 @@ background = cv.bitwise_and(gray, gray, mask = mask_inv)
 background = np.stack((background,)*3, axis=-1)
 added_img = cv.add(res, background)
 
-cv.namedWindow("res", cv.WINDOW_NORMAL)
-cv.namedWindow("hsv", cv.WINDOW_NORMAL)
-cv.namedWindow("mask", cv.WINDOW_NORMAL)
+# cv.namedWindow("res", cv.WINDOW_NORMAL)
+# cv.namedWindow("hsv", cv.WINDOW_NORMAL)
+# cv.namedWindow("mask", cv.WINDOW_NORMAL)
 cv.namedWindow("added", cv.WINDOW_NORMAL)
-cv.namedWindow("back", cv.WINDOW_NORMAL)
-cv.namedWindow("mask_inv", cv.WINDOW_NORMAL)
-cv.namedWindow("gray", cv.WINDOW_NORMAL)
+# cv.namedWindow("back", cv.WINDOW_NORMAL)
+# cv.namedWindow("mask_inv", cv.WINDOW_NORMAL)
+# cv.namedWindow("gray", cv.WINDOW_NORMAL)
 
-cv.imshow("back", background)
-cv.imshow("mask_inv", mask_inv)
+# cv.imshow("back", background)
+# cv.imshow("mask_inv", mask_inv)
 cv.imshow("added",added_img)
-cv.imshow("mask", mask)
-cv.imshow("gray", gray)
-cv.imshow("hsv", hsv)
-cv.imshow("res", res)
+# cv.imshow("mask", mask)
+# cv.imshow("gray", gray)
+# cv.imshow("hsv", hsv)
+# cv.imshow("res", res)
 
 if cv.waitKey(0):
     cv.destroyAllWindows()
